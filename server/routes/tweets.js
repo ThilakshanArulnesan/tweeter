6d735dbc1a80bfa8b8d54c3e42f43ac188602fb1
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-10-07 17:58:25 +0000

    Added nav bar and header styling

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,28 +1,30 @@
 "use strict";
 
-const userHelper    = require("../lib/util/user-helper")
+const userHelper = require("../lib/util/user-helper")
 
-const express       = require('express');
-const tweetsRoutes  = express.Router();
+const express = require('express');
+const tweetsRoutes = express.Router();
 
 module.exports = function(DataHelpers) {
 
   tweetsRoutes.get("/", function(req, res) {
+    //Gets a ll tweets
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
         res.status(500).json({ error: err.message });
       } else {
-        res.json(tweets);
+        res.json(tweets);//Provides as a JSON
       }
     });
   });
 
   tweetsRoutes.post("/", function(req, res) {
+    //Posts a new tweet
     if (!req.body.text) {
-      res.status(400).json({ error: 'invalid request: no data in POST body'});
+      res.status(400).json({ error: 'invalid request: no data in POST body' });
       return;
     }
-
+    //Checks for a username, assigns a random one else
     const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
     const tweet = {
       user: user,
