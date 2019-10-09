16c5bcb1decbc2f586865c880e521950f14e8816
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-10-09 21:23:53 +0000

    Styled buttons, fixed nav bar positioning

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
   const MAX_CHARS = 140;
   console.log("Document is ready");
   $(".new-tweet textarea").on('input', function() {
-    console.log(this);
-    console.log($(this).val());
+
     let len = $(this).val().length;
     let charsLeft = MAX_CHARS - len;
     let theCounter = $(this).nextAll(".counter");
